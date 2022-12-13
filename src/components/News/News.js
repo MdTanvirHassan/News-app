e7b1ec65e2a37c77b9523f074d58a5aaa989e72b
Tanvir Hassan <Tanvirtamim72@gmail.com>
2022-12-13 22:28:24 +0600

    added next & previous button.

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -4,18 +4,42 @@ import NewsItem from '../NewsItem/NewsItem'
 export class News extends Component {
     constructor(){
         super();
-        console.log('news item') 
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page:1
         }  
     }
     async componentDidMount(){
-        let url='https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3d202c609e0e4308afa54e2d4c76721f';
+        let url='https://newsapi.org/v2/top-headlines?country=us&apiKey=3d202c609e0e4308afa54e2d4c76721f&page=1&pageSize=18';
         let data = await fetch(url)
         let parseData =await data.json();
-        this.setState({articles: parseData.articles});
+        this.setState({articles: parseData.articles, totalResults: parseData.totalResults});
     }
+    handlePreClick= async ()=> {
+      console.log("Pre-click");
+      let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=3d202c609e0e4308afa54e2d4c76721f&page=${this.state.page-1}&pageSize=18`;
+        let data = await fetch(url)
+        let parseData =await data.json();
+        this.setState({
+          page: this.state.page - 1,
+          articles: parseData.articles});
+      }
+    
+
+    handleNextClick= async ()=> {
+      console.log("Next-click")
+      this.next= this.state.totalResults/18
+      if(this.state.totalResults/18){
+        let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=3d202c609e0e4308afa54e2d4c76721f&page=${this.state.page+1}&pageSize=18`;
+        let data = await fetch(url)
+        let parseData =await data.json();
+        this.setState({
+          page: this.state.page+1,
+          articles: parseData.articles});
+      }
+    }
+
   render() {
     return (
       <div className='container my-3'>
@@ -24,12 +48,16 @@ export class News extends Component {
             {this.state.articles.map((element) =>{
                 return (
                     <div className="col-md-4 my-1 p-1" key={element.url}>
-                        <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                        <NewsItem title={element.title} description={element.description}
+                         imageUrl={element.urlToImage} newsUrl={element.url}/>
                     </div>
                 )
-            })
-            }
-            
+             })
+            }     
+        </div>
+        <div className="d-flex justify-content-between mt-5">
+          <button disabled={this.state.page<=1} className='btn btn-dark' type='button' onClick={this.handlePreClick}>&larr; Previous</button>
+          <button disabled={this.state.page===this.next+1 }className='btn btn-dark' type='button' onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
--- src/components/NewsItem/NewsItem.js
+++ src/components/NewsItem/NewsItem.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react'
+import './NewsItem.css'
 
 export class NewsItem extends Component {
-    constructor(){
-        super();
-        console.log('news item')
-        
-    }
   render() {
-    let {title,description,imageUrl,newsUrl} = this.props
+    let {title, description, imageUrl, newsUrl} = this.props
     return (
       <div>
-        <div className="card" style={{width: "20rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..." style={{height: "150px"}}/>
-            <div className="card-body"style={{height: "300px"}}>
-                <h6 className="card-title" style={{height: "100px"}}>{title}</h6>
+        <div className="card" style={{width: "22rem"}}>
+            <img src={imageUrl} className="card-img-top" alt="..." style={{height: "175px"}}/>
+            <div className="card-body"style={{height: "280px"}}>
+                <h6 className="card-title" style={{height: "80px"}}>{title}</h6>
                 <p className="card-text" style={{height: "100px", overflow:"hidden"}}>{description}</p>
                 <a href={newsUrl} target="blank" className="btn btn-sm btn-outline-primary"> Read more...</a>
             </div>
