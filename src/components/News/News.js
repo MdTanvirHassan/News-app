27c9d0becec283defe3152fe8d318aae626fff37
Tanvir Hassan <Tanvirtamim72@gmail.com>
2022-12-21 18:03:49 +0600

    On clicking update title

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -14,13 +14,14 @@ export class News extends Component {
     pageSize: PropTypes.number,
     category: PropTypes.string,
   };
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page: 1,
     };
+    document.title = `NewsTime -${this.capitalizeFirstLetter(this.props.category)}`
   }
   async updateNews() {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3d202c609e0e4308afa54e2d4c76721f&page=${this.state.page}&pageSize=${this.props.pageSize}`;
@@ -36,6 +37,10 @@ export class News extends Component {
   async componentDidMount() {
     this.updateNews();
   }
+
+  capitalizeFirstLetter=(string)=> {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
   handlePreClick = async () => {
     this.setState({ page: this.state.page - 1 });
     this.updateNews();
@@ -50,7 +55,7 @@ export class News extends Component {
     return (
       <>
         <div className="container my-3">
-          <h4 className="text-center text-success">NewsTime - Top Headlines</h4>
+          <h4 className="text-center text-success">NewsTime - Top Headlines from {this.capitalizeFirstLetter(this.props.category)}</h4>
           <hr />
           {this.state.loading && <Snipper />}
           <div className="row">
--- src/components/Snipper/Snipper.js
+++ src/components/Snipper/Snipper.js
@@ -5,7 +5,7 @@ export default class Snipper extends Component {
   render() {
     return (
       <div className='text-center'>
-        <img src={loading} alt="Loading" />
+        <img src={loading} alt="Loading" width='35%' />
       </div>
     )
   }
